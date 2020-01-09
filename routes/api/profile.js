b818f84f32a0173420eeb32a3ca8283c8f916ba7
NOP-slide <phelps.andrewj@gmail.com>
2020-01-09 14:41:25 +1100

    added register user

---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/profile
+// @desc    Test route
+// @access  Public
+router.get('/', (req, res) => res.send('This is the profile route'));
+
+module.exports = router;
